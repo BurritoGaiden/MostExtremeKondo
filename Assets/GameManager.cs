88ba1209ba0b914fedf3f8cab6e17161d8950dcf
Nesfero <thenesfero@gmail.com>
2019-01-27 10:35:20 -0800

    Pushin Animations and Graphics before Design sync

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -23,6 +23,14 @@ public class GameManager : MonoBehaviour
     public AudioClip[] audioClips;
     public AudioSource musicSource;
     public GameObject slapEffect;
+    public AudioSource jetSetSource;
+    public AudioSource crowdSource;
+
+    public GameObject goodBlip;
+    public GameObject theCamera;
+
+    public GameObject conLeft;
+    public GameObject conRight;
 
     public void Start()
     {
@@ -68,6 +76,10 @@ public class GameManager : MonoBehaviour
         hand.GetComponent<Animator>().Play("Hand_Slap");
         GiveRandomPitch(1f, 2.5f);
         GetComponent<AudioSource>().PlayOneShot(audioClips[0]);
+        jetSetSource.pitch = 1;
+        jetSetSource.PlayOneShot(audioClips[7]);
+        //crowdSource.PlayOneShot(audioClips[12]);
+        goodBlip.GetComponent<Animator>().Play("Good_Blip_Flash");
         thisAnim = ObjectAnimState.ObjOut;
         currentWaitTime = waitTime;
     }
@@ -105,26 +117,40 @@ public class GameManager : MonoBehaviour
         print(rRand);
         if (rRand == 0) { 
             hand.GetComponent<Animator>().Play("Hand_Fold");
+            theCamera.GetComponent<Animator>().Play("Camera_LeftMovement");
             GiveRandomPitch(1f, 2.5f);
             GetComponent<AudioSource>().PlayOneShot(audioClips[3]);
+            jetSetSource.pitch = .6f;
+            jetSetSource.PlayOneShot(audioClips[7]);
         }
         else if (rRand == 1) {
             hand.GetComponent<Animator>().Play("Hand_Fold1");
+            theCamera.GetComponent<Animator>().Play("Camera_UpMovement");
             GiveRandomPitch(1f, 2.5f);
             GetComponent<AudioSource>().PlayOneShot(audioClips[4]);
+            jetSetSource.pitch = .7f;
+            jetSetSource.PlayOneShot(audioClips[7]);
         }
         else if (rRand == 2) {
             hand.GetComponent<Animator>().Play("Hand_Fold2");
+            theCamera.GetComponent<Animator>().Play("Camera_DownMovement");
             GiveRandomPitch(1f, 2.5f);
             GetComponent<AudioSource>().PlayOneShot(audioClips[5]);
+            jetSetSource.pitch = .9f;
+            jetSetSource.PlayOneShot(audioClips[7]);
         }
         else if (rRand == 3) {
             hand.GetComponent<Animator>().Play("Hand_Fold3");
+            theCamera.GetComponent<Animator>().Play("Camera_RightMovement");
             GiveRandomPitch(1f, 2.5f);
             GetComponent<AudioSource>().PlayOneShot(audioClips[6]);
+            jetSetSource.pitch = .8f;
+            jetSetSource.PlayOneShot(audioClips[7]);
             //currentWaitTime = waitTime;
         }
 
+        animeVignette.GetComponent<Animator>().Play("Anime_Vignette_Off");
+        animeVignette.GetComponent<Animator>().Play("Anime_Vignette_Flashing");
         slapEffect.GetComponent<Animator>().Play("Slap_Effect_Blip");
         //give object model a rotation to rotate to
         to = rotations[Random.Range(0, rotations.Length)];
